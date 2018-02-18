bbeb3cfeab37b5d5d33f05c0a40cc6418a745014
Sachin Nagpal <sachinn@192.168.10.112>
2018-02-19 01:58:09 +0530

    pre-final

--- script.js
+++ script.js
@@ -51,7 +51,7 @@
      return;
    }
    var j;
-   queue.shift();
+   var num = queue.shift();
    for(j = 0;j < qindex-1;j++) {
      var data = document.getElementById(9-j);
      var sdata = document.getElementById(9-j-1);
@@ -63,5 +63,8 @@
    cd.remove();
    filled[9-j] = false;
    qindex--;
-
+   var node = document.createElement("p");
+   var textnode = document.createTextNode(num);
+   node.appendChild(textnode);
+   document.getElementById("con").appendChild(node);
  }
